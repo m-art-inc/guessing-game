255feba640573800925064fb49cda637510ae1fb
martincarmen03 <martincarmen03@gmail.com>
2016-07-04 21:13:21 -0700

    added toLowerCase function to yes/no questions

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ console.log("Message: "+message);
 
 var guessOne = prompt("Do I own a dog?");
   console.log("Question (1)");
-if (guessOne == "yes"){
+if (guessOne.toLowerCase() == "yes"){
 alert("Yes, "+ userName +". In fact her name is Luna and she is 3 years old!");
   console.log("User guess was correct. Your guess was "+guessOne);
   document.write("<p style=color:green;>Question (1) Correct, your guess was "+guessOne+",</P>");
@@ -18,7 +18,7 @@ alert("Sorry, "+ userName +" it's true, I own a dog");
 
 var guessTwo = prompt("Did I grow up in Oregon?");
   console.log("Question (2)");
-if (guessTwo == "no"){
+if (guessTwo.toLowerCase() == "no"){
 alert(userName +". You are right! I did NOT grow up in Oregon");
   console.log("User guess was correct.");
   document.write("<p style=color:green;>Question (2) Correct, your guess was "+guessTwo+", </p>");
@@ -30,7 +30,7 @@ alert("Sorry, "+ userName +" I did NOT grow up in Oregon.");
 
 var guessThree = prompt("Do I speak more than one language?");
   console.log("Question (3)");
-if (guessThree == "yes"){
+if (guessThree.toLowerCase() == "yes"){
 alert(userName +". You are right! I speak English and Spanish!");
   console.log("User guess was correct.");
   document.write("<p style=color:green;>Question (3) Correct, your guess was "+guessThree+".</p>");
@@ -40,18 +40,18 @@ alert(userName +". It's true! I speak English and Spanish!");
   document.write("<p style=color:red;>Question (3) Incorrect, your guess was "+guessThree+".</p>");
 }
 
-var guessFour = prompt("What Is My Favorite Number? - (between 1 - 10)");
+var guess = prompt("What Is My Favorite Number? - (between 1 - 15)");
   console.log("Question (Bonus)");
-if (guessFour == 3){
+if (guess == 3){
   console.log("User guess was correct.");
   document.write("<p style=color:green;>Bonus Question (4) Correct, your guess was "+guessFour+".</p>");
 }
-if (guessFour > 4){
-  guessFour = prompt("Your guess is too high!");
+if (guess > 4){
+  guess = prompt("Your guess is too high!");
   console.log("User guess was too high!");
   document.write("<p>Question (4) Your guess was too high!.</p>");
-} else if (guessFour < 3){
-  guessFour = prompt("Your guess is too low!");
+} else if (guess < 3){
+  guess = prompt("Your guess is too low!");
   console.log("User guess was too low!");
   document.write("<p>Question (4) Your guess was too low!.</p>");
 }
